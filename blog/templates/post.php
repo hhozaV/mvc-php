266a1c4377208950f0d4eb48deb00da979c1c1a9
Vazohh <vazohhbtw@gmail.com>
2023-10-18 15:16:40 +0200

    Ajoutez des commentaires

--- blog/templates/post.php
+++ blog/templates/post.php
@@ -16,6 +16,22 @@
                 <em>le <?= $post['french_creation_date'] ?></em>
             </h3>
 
+            <h2>Commentaires</h2>
+            <form action="index.php?action=addComment&id=<?= $post['identifier'] ?>" method="post">
+                <div>
+                    <label for="author">Auteur</label><br />
+                    <input type="text" id="author" name="author" />
+                </div>
+                <div>
+                    <label for="comment">Commentaire</label><br />
+                    <textarea id="comment" name="comment"></textarea>
+                </div>
+                <div>
+                    <input type="submit" />
+                </div>
+            </form>
+
+
             <p>
                 <?= nl2br(htmlspecialchars($post['content'])) ?>
             </p>
